74c8df6952b386a468a3fd2b281cbd04e87476b7
Yuhan Fang <yuhan@eastrocklabs.com>
2022-10-18 23:35:09 +0000

    add USDA markets

--- aptos/api/aux-ts/examples/mainnet-init.ts
+++ aptos/api/aux-ts/examples/mainnet-init.ts
@@ -17,6 +17,7 @@ const USDC = "0x5e156f1207d0ebfa19a9eeff00d62a282278fb8719f4fab3a586a0a2c0fffbea
 const SOL = "0xdd89c0e695df0692205912fb69fc290418bed0dbe6e4573d744a6d5e6bab6c13::coin::T";
 const WETH = "0xcc8a89c8dce9693d354449f1f73e60e14e347417854f029db5bc8e7454008abb::coin::T";
 const WBTC = "0xae478ff7d83ed072dbc5e264250e67ef58f57c99d89b447efd8a0a2e8b2be76e::coin::T";
+const USDA = "0x1000000fa32d122c18a6a31c009ce5e71674f22d06a581bb0a15575e6addadcc::usda::USDA";
 
 const MARKETS = [
   {
@@ -28,6 +29,12 @@ const MARKETS = [
   {
     base: WETH,
     quote: USDC,
+    baseLotSize: "0.001",
+    quoteLotSize: "0.01",
+  },
+  {
+    base: WETH,
+    quote: USDA,
     baseLotSize: "0.001",
     quoteLotSize: "0.01",
   },
@@ -54,6 +61,11 @@ const POOLS = [
   {
     base: WETH,
     quote: USDC,
+    feeBps: "30",
+  },
+  {
+    base: WETH,
+    quote: USDA,
     feeBps: "30",
   },
   {
